fd4c881baf8a3a2d87ed10f69d58cd60c90ed0e3
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2026-09-21 23:01:45 +0530

    Use parser in shell loop

--- src/main.c
+++ src/main.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
 #include <string.h>
 #include "shell.h"
+#include "parser.h"
 
 int main(void)
 {
     char line[1024];
+    char *args[MAX_ARGS];
 
-    while (1) {
+    while (1)
+    {
         printf(PROMPT);
         fflush(stdout);
 
-        if (fgets(line, sizeof(line), stdin) == NULL) {
+        if (fgets(line, sizeof(line), stdin) == NULL)
+        {
             printf("\n");
             break;
         }
 
         line[strcspn(line, "\n")] = '\0';
 
-        if (line[0] == '\0')
-            continue;
-
         if (strcmp(line, "exit") == 0)
+        {
             break;
+        }
 
-        printf("You typed: %s\n", line);
+        int count = parse(line, args);
+        if (count == 0)
+        {
+            continue;
+        }
+
+        for (int i = 0; args[i] != NULL; i++)
+        {
+            printf("args[%d] = %s\n", i, args[i]);
+        }
     }
 
     return 0;
