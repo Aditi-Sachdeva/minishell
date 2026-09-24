d4b72b88812138bf7b13b6164c0007894708b03b
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2026-09-24 19:33:24 +0530

    Add redirection parsing to detect <, >, and >> operators

--- include/parser.h
+++ include/parser.h
@@ -3,6 +3,14 @@
 
 #define MAX_ARGS 64
 
+typedef struct
+{
+    char *input_file;
+    char *output_file;
+    int append;
+} redirect_t;
+
 int parse(char *line, char *args[]);
+void extract_redirection(char *args[], redirect_t *r);
 
 #endif
--- src/parser.c
+++ src/parser.c
@@ -16,3 +16,54 @@ int parse(char *line, char *args[])
     args[count] = NULL;
     return count;
 }
+
+void extract_redirection(char *args[], redirect_t *r)
+{
+
+    r->input_file = NULL;
+    r->output_file = NULL;
+    r->append = 0;
+
+    char *clean[MAX_ARGS];
+    int j = 0;
+
+    for (int i = 0; args[i] != NULL; i++)
+    {
+        if (strcmp(args[i], "<") == 0)
+        {
+            if (args[i + 1] != NULL)
+            {
+                r->input_file = args[i + 1];
+                i++;
+            }
+        }
+        else if (strcmp(args[i], ">") == 0)
+        {
+            if (args[i + 1] != NULL)
+            {
+                r->output_file = args[i + 1];
+                r->append = 0;
+                i++;
+            }
+        }
+        else if (strcmp(args[i], ">>") == 0)
+        {
+            if (args[i + 1] != NULL)
+            {
+                r->output_file = args[i + 1];
+                r->append = 1;
+                i++;
+            }
+        }
+        else
+        {
+            clean[j++] = args[i];
+        }
+    }
+    clean[j] = NULL;
+
+    for (int k = 0; k <= j; k++)
+    {
+        args[k] = clean[k];
+    }
+}
