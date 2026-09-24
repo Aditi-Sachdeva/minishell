e424f80568a39dfb23d5a87c31766fdc8664a0fa
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2026-09-24 19:34:30 +0530

    Integrate redirection into shell loop before running commands

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include "shell.h"
 #include "parser.h"
-#include "executor.h"
 #include "builtins.h"
+#include "executor.h"
 
 int main(void)
 {
@@ -23,29 +25,50 @@ int main(void)
 
         line[strcspn(line, "\n")] = '\0';
 
-        if (strcmp(line, "exit") == 0)
-        {
-            break;
-        }
-
         int count = parse(line, args);
         if (count == 0)
-        {
             continue;
-        }
 
         if (strcmp(args[0], "exit") == 0)
-        {
             break;
-        }
+
+        redirect_t r;
+        extract_redirection(args, &r);
+
+        if (args[0] == NULL) /* line was only an operator */
+            continue;
 
         if (is_builtin(args[0]))
         {
+            int saved_stdout = -1;
+
+            if (r.output_file != NULL)
+            {
+                int flags = O_WRONLY | O_CREAT | (r.append ? O_APPEND : O_TRUNC);
+                int fd = open(r.output_file, flags, 0644);
+                if (fd < 0)
+                {
+                    perror(r.output_file);
+                }
+                else
+                {
+                    saved_stdout = dup(1);
+                    dup2(fd, 1);
+                    close(fd);
+                }
+            }
+
             run_builtin(args);
+
+            if (saved_stdout != -1)
+            {
+                dup2(saved_stdout, 1);
+                close(saved_stdout);
+            }
         }
         else
         {
-            run_external(args);
+            run_external(args, &r);
         }
     }
 
